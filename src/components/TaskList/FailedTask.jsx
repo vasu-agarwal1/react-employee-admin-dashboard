0752d05ffc636ebad7831a4b6ea3c94ce0a51654
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-07-23 23:27:04 +0530

    created failed task component

---
+++ src/components/TaskList/FailedTask.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const FailedTask = () => {
+  return (
+    <div className='flex-shrink-0 h-full w-[300px] p-5 bg-fuchsia-400 rounded-xl'>
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>19 July 2025 </h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam exercitationem doloribus itaque voluptates! Magni, distinctio?
+            </p>
+            <div className='mt-2'>
+                <button className='w-full'>Task Failed</button>
+            </div>
+        </div>
+  )
+}
+
+export default FailedTask
