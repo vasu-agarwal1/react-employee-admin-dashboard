b1cfbf98078a1243869a011a0012df2c9dceb248
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-07-24 23:28:27 +0530

    mapped data in completed task component

--- src/components/TaskList/CompleteTask.jsx
+++ src/components/TaskList/CompleteTask.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const CompleteTask = () => {
+const CompleteTask = ({data}) => {
   return (
     <div className='flex-shrink-0 h-full w-[300px] p-5 bg-emerald-400 rounded-xl'>
             <div className='flex justify-between items-center'>
-                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-                <h4 className='text-sm'>19 July 2025 </h4>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
+                <h4 className='text-sm'>{data.date}</h4>
             </div>
-            <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+            <h2 className='mt-5 text-2xl font-semibold'>{data.title}</h2>
             <p className='text-sm mt-2'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam exercitationem doloribus itaque voluptates! Magni, distinctio?
+                {data.description}
             </p>
             <div className='mt-2'>
-                <button className='w-full'>Task Completed</button>
+                <button className='w-full bg-cyan-600 rounded font-medium py-1 px-2 text-xs'>Task Completed</button>
             </div>
         </div>
   )
