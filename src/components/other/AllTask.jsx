566be6b6cd8cda1fa4c2d0c361c301f05b80547e
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-07-24 23:31:24 +0530

    added useContext hook

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -1,37 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../context/AuthProvider'
 
 const AllTask = () => {
+
+    const authData = useContext(AuthContext)
+    
+
+    
   return (
-    <div className='bg-[#1c1c1c] rounded mt-5 p-5 h-48 overflow-auto'>
-        <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>Vasu</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
+    
+    <div className='bg-[#1c1c1c] rounded mt-5 p-5  '>
+         <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2 className='w-1/5'>Employee Name</h2>
+            <h3 className='w-1/5'>New Task</h3>
+            <h5 className='w-1/5'>Active Task</h5>
+            <h5 className='w-1/5'>Completed</h5>
+            <h5 className='w-1/5'>Failed</h5>
+
         </div>
 
-        <div className='bg-blue-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>Vasu</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
+        <div>
+            {authData.employees.map((elem)=>{
+            return(
+            <div className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2 className='w-1/5 '>{elem.firstName}</h2>
+            <h3 className='w-1/5 text-blue-400'>{elem.taskCount.newTask}</h3>
+            <h5 className='w-1/5 text-yellow-400'>{elem.taskCount.active}</h5>
+            <h5 className='w-1/5 text-white'>{elem.taskCount.completed}</h5>
+            <h5 className='w-1/5 text-red-600'>{elem.taskCount.failed}</h5>
         </div>
+            )
 
-        <div className='bg-emerald-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>Vasu</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
+        })}
         </div>
+        
 
-        <div className='bg-fuchsia-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>Vasu</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
-        </div>
-
-        <div className='bg-orange-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2>Vasu</h2>
-            <h3>Make a UI Design</h3>
-            <h5>Status</h5>
-        </div>
+        
 
         
       
