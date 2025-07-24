d53536cef6de7b968430fb6dd575c39f1afd9343
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-07-24 23:27:37 +0530

    mapped data in accept task component

--- src/components/TaskList/AcceptTask.jsx
+++ src/components/TaskList/AcceptTask.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
-const AcceptTask = () => {
+const AcceptTask = ({data}) => {
+    
   return (
      <div className='flex-shrink-0 h-full w-[300px] p-5 bg-red-400 rounded-xl'>
             <div className='flex justify-between items-center'>
-                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-                <h4 className='text-sm'>19 July 2025 </h4>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
+                <h4 className='text-sm'>{data.date} </h4>
             </div>
-            <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+            <h2 className='mt-5 text-2xl font-semibold'>{data.title}</h2>
             <p className='text-sm mt-2'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam exercitationem doloribus itaque voluptates! Magni, distinctio?
+                {data.description}
             </p>
             <div className='flex justify-between mt-4'>
-                <button className='bg-emerald-500 py-1 px-2 text-sm'>Mark as Completed</button>
-                <button className='bg-red-500 py-1 px-2 text-sm'>Mark as Failed</button>
+                <button className='bg-emerald-500 rounded font-medium py-1 px-2 text-sm'>Mark as Completed</button>
+                <button className='bg-red-500 rounded font-medium py-1 px-2 text-sm'>Mark as Failed</button>
             </div>
         </div>
   )
