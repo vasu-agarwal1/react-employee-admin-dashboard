6397f55deb6c8b235b116f27342b7f499cafedfb
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-07-24 23:33:27 +0530

    passed data to each component

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -4,14 +4,25 @@ import NewTask from './NewTask'
 import CompleteTask from './CompleteTask'
 import FailedTask from './FailedTask'
 
-const TaskList = () => {
+const TaskList = ({data}) => {
+  
   return (
     <div id='tasklist' className='h-[55%] overflow-x-auto  flex items-center justify-start gap-5 w-full py-5  mt-10'>
-          <AcceptTask />
-          <NewTask />
-          <CompleteTask />
-          <FailedTask />
-      
+          {data.tasks.map((elem, idx)=>{
+            
+            if(elem.active){
+              return<AcceptTask key={idx} data={elem}/>
+            }
+            if(elem.NewTask){
+              return<NewTask key={idx} data = {elem}/>
+            }
+            if(elem.completed){
+              return<CompleteTask key={idx} data = {elem}/>
+            }
+            if(elem.failed){
+              return<FailedTask key={idx} data = {elem}/>
+            }
+          })}
     </div>
   )
 }
