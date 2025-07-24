24345ad958c10c3ebee483681324b6a5a69b5d70
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-07-24 23:30:34 +0530

    mapped data in new task component

--- src/components/TaskList/NewTask.jsx
+++ src/components/TaskList/NewTask.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
-const NewTask = () => {
+const NewTask = ({data}) => {
   return (
     <div className='flex-shrink-0 h-full w-[300px] p-5 bg-cyan-400 rounded-xl'>
             <div className='flex justify-between items-center'>
-                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-                <h4 className='text-sm'>19 July 2025 </h4>
+                <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
+                <h4 className='text-sm'>{data.date} </h4>
             </div>
-            <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
+            <h2 className='mt-5 text-2xl font-semibold'>{data.title}</h2>
             <p className='text-sm mt-2'>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam exercitationem doloribus itaque voluptates! Magni, distinctio?
+                {data.description}
             </p>
             <div className='mt-4'>
-                <button>Accept Task</button>
+                <button className='bg-emerald-500 rounded font-medium py-1 px-2 text-xs'>Accept Task</button>
             </div>
         </div>
   )
