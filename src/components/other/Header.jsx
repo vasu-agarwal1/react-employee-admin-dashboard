7c29cff093008aab8d54a5fbd6699888297da6c7
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-07-25 23:27:21 +0530

    Added funtionality to logout button

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
+import { setLocalStorage } from '../../utils/localStorage'
 
-function Header({data}) {
-  console.log(data)
+function Header(props) {
+
+  // const[username, setUsername] = useState('')
+
+  // if(!data){
+  //   setUsername('Admin')
+  // }else{
+  //   setUsername(data.firstName)
+  // }
+
+  const logOutUser = ()=>{
+    localStorage.setItem('loggedInUser', '')//removes loggedinuser data from local storage of that windw
+    props.changeUser('')//when logout button is clicked it sets user as empty which opens the login page
+    // window.location.reload()//reloads the window on click
+  }
+  
   return (
     
         <div className='flex items-end justify-between'>
-            <h1 className='text-2xl font-medium'>Hello <br /> <span className='font-semibold text-3xl'> {data.firstName} 👋</span> </h1>
-            <button className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded'>Log Out</button>
+            <h1 className='text-2xl font-medium'>Hello <br /> <span className='font-semibold text-3xl'> Vasu 👋</span> </h1>
+            <button onClick={logOutUser} className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded'>Log Out</button>
         </div>
       
     
