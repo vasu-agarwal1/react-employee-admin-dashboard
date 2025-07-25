a8393b6faeae735cdb80852aeb5f77073824beca
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-07-25 23:33:53 +0530

    added some css

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -11,23 +11,23 @@ const AllTask = () => {
     
     <div className='bg-[#1c1c1c] rounded mt-5 p-5  '>
          <div className='bg-red-400 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2 className='w-1/5'>Employee Name</h2>
-            <h3 className='w-1/5'>New Task</h3>
-            <h5 className='w-1/5'>Active Task</h5>
-            <h5 className='w-1/5'>Completed</h5>
-            <h5 className='w-1/5'>Failed</h5>
+            <h2 className='text-lg font-medium w-1/5'>Employee Name</h2>
+            <h3 className='text-lg font-medium w-1/5'>New Task</h3>
+            <h5 className='text-lg font-medium w-1/5'>Active Task</h5>
+            <h5 className='text-lg font-medium w-1/5'>Completed</h5>
+            <h5 className='text-lg font-medium w-1/5'>Failed</h5>
 
         </div>
 
         <div>
-            {authData.employees.map((elem)=>{
+            {authData.employees.map((elem, idx)=>{
             return(
-            <div className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
-            <h2 className='w-1/5 '>{elem.firstName}</h2>
-            <h3 className='w-1/5 text-blue-400'>{elem.taskCount.newTask}</h3>
-            <h5 className='w-1/5 text-yellow-400'>{elem.taskCount.active}</h5>
-            <h5 className='w-1/5 text-white'>{elem.taskCount.completed}</h5>
-            <h5 className='w-1/5 text-red-600'>{elem.taskCount.failed}</h5>
+            <div key={idx} className='border-2 border-emerald-500 mb-2 py-2 px-4 flex justify-between rounded'>
+            <h2 className='text-lg font-medium w-1/5 '>{elem.firstName}</h2>
+            <h3 className='text-lg font-medium w-1/5 text-blue-400'>{elem.taskCount.newTask}</h3>
+            <h5 className='text-lg font-medium w-1/5 text-yellow-400'>{elem.taskCount.active}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-white'>{elem.taskCount.completed}</h5>
+            <h5 className='text-lg font-medium w-1/5 text-red-600'>{elem.taskCount.failed}</h5>
         </div>
             )
 
