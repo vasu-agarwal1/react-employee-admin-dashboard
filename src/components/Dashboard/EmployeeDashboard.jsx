635e2d735956201310468a0f2da955ef0e41aaa7
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-07-25 23:24:48 +0530

    Added some props

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -3,14 +3,14 @@ import Header from '../other/Header'
 import TaskListNumber from '../other/TaskListNumber'
 import TaskList from '../TaskList/TaskList'
 
-const EmployeeDashboard = ({data}) => {
+const EmployeeDashboard = (props) => {
 
   return (
     <div className='p-10 bg-[#1C1C1C] h-screen'>
       
-        <Header data={data} />
-        <TaskListNumber data={data}  />
-        <TaskList data={data}  />
+        <Header changeUser={props.changeUser} data={props.data} />
+        <TaskListNumber data={props.data}  />
+        <TaskList data={props.data}  />
     </div>
   )
 }
