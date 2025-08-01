62ab0938af39537f9f6259ec6e0f3e8ed2e4f2b9
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-01 22:45:48 +0530

    passed some props

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -21,7 +21,7 @@ function Header(props) {
   return (
     
         <div className='flex items-end justify-between'>
-            <h1 className='text-2xl font-medium'>Hello <br /> <span className='font-semibold text-3xl'> Vasu 👋</span> </h1>
+            <h1 className='text-2xl font-medium'>Hello <br /> <span className='font-semibold text-3xl'> {props.firstName} 👋</span> </h1>
             <button onClick={logOutUser} className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded'>Log Out</button>
         </div>
       
