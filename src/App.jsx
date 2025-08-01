b20a6ef8cd2d304fe95c1fbf5925e32189ea1a6f
Vasu Agarwal <agarwal.vasu32@gmail.com>
2025-08-02 00:36:08 +0530

    chenged authdata to  userdata

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
 
   const[user, setUser] = useState(null)
   const[loggedInUserData, setloggedInUserData] = useState(null)
-  const authData = useContext(AuthContext)
+  const [userData, setUserData] = useContext(AuthContext)
 
   useEffect(()=>{
     const loggedInUser = localStorage.getItem('loggedInUser')
@@ -35,8 +35,8 @@ const App = () => {
     if(email == 'admin@me.com' && password == '123'){
       setUser('admin')
       localStorage.setItem('loggedInUser',JSON.stringify({role: 'admin'}) )
-    }else if(authData){
-      const employee = authData.employees.find((e) =>email == e.email && e.password == password)
+    }else if(userData){
+      const employee = userData.find((e) =>email == e.email && e.password == password)
       if(employee){
       setUser('employee')
       setloggedInUserData(employee)
